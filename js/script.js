418e8a044baf8a1e731046f2cb92bcb8596a88e7
Yan_Pustynnyy <necrosith@yandex.ru>
2018-11-06 21:32:58 +0400

    Difference page functionality added

--- js/script.js
+++ js/script.js
@@ -338,5 +338,52 @@ window.addEventListener('DOMContentLoaded', function() {
         this.value = this.value.replace (/[^0-9/.]/, '');
     });
 
+    // Functionality for the difference field
 
-});
+    let btnShow1 = document.querySelector('.button-plus-1'),
+        btnShow2 = document.querySelector('.button-plus-2'),
+        parentBlock1 = document.querySelector('.officerold'),
+        parentBlock2 = document.querySelector('.officernew');
+
+        btnShow1.addEventListener('click', function() {
+            addNewBlock(this, parentBlock1);
+        });
+
+        btnShow2.addEventListener('click', function() {
+            addNewBlock(this, parentBlock2);
+        });
+
+        function addNewBlock(element, parentBlock) {
+            let childBlocks = 0;
+            for (let i = 0; i < parentBlock.childNodes.length; i++) {
+                if (parentBlock.childNodes[i].className == 'officer__card-item diff-card') {
+                    childBlocks++;
+                }
+            }
+            let cardWrapper = document.createElement('div'),
+                cardTitle = document.createElement('div'),
+                cardDescription = document.createElement('div');
+
+            cardWrapper.classList.add('officer__card-item');
+            cardWrapper.classList.add('diff-card');
+            cardTitle.classList.add('card__counter');
+            cardDescription.classList.add('card__descr');
+
+            console.log(childBlocks);
+            cardTitle.textContent = '0' + (childBlocks+1);
+            cardWrapper.appendChild(cardTitle);
+            if (childBlocks == 0) {
+               cardDescription.textContent = 'First step with some text and explanation';         
+            }
+            if (childBlocks == 1) {
+                cardDescription.textContent = 'Second step with some text and explanation';
+             }
+             if (childBlocks == 2) {
+                cardDescription.textContent = 'Third step with some text and explanation';
+                element.style.display = 'none';
+             }
+            cardWrapper.appendChild(cardDescription); 
+            parentBlock.insertBefore(cardWrapper, parentBlock.children[parentBlock.childNodes.length-4]);
+            console.log(parentBlock.childNodes.length-4);
+        }
+    });
